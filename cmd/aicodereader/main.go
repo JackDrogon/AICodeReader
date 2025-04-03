00fddd45f7001562a6d2e25c54cac8927cd2baf6
Jack Drogon <jack.xsuperman@gmail.com>
2025-04-03 11:05:17 +0800

    Fix openai-go dep to support deepseek reasoning
    Signed-off-by: Jack Drogon <jack.xsuperman@gmail.com>

--- cmd/aicodereader/main.go
+++ cmd/aicodereader/main.go
@@ -55,6 +55,10 @@ func test_standard_request(config Config) {
 		log.Printf("ChatCompletion error: %v\n", err)
 		return
 	}
+	fmt.Println("----- 推理过程  -----")
+	fmt.Println(resp.Choices[0].Message.ReasoningContent)
+
+	fmt.Println("----- 最终回答 -----")
 	fmt.Println(resp.Choices[0].Message.Content)
 }
 
@@ -80,6 +84,8 @@ func test_stream_request(config Config) {
 					Content: "常见的十字花科植物有哪些？",
 				},
 			},
+			Temperature: 0.7,
+			Stream:      true,
 		},
 	)
 	if err != nil {
@@ -87,6 +93,8 @@ func test_stream_request(config Config) {
 		return
 	}
 	defer stream.Close()
+
+	isThinking := false
 
 	for {
 		recv, err := stream.Recv()
@@ -100,7 +108,28 @@ func test_stream_request(config Config) {
 		}
 
 		if len(recv.Choices) > 0 {
-			fmt.Print(recv.Choices[0].Delta.Content)
+			if recv.Choices[0].Delta.ToolCalls != nil ||
+				(recv.Choices[0].Delta.Role == "assistant" && !isThinking) {
+				if !isThinking {
+					fmt.Println("----- 模型思考过程 -----")
+					isThinking = true
+				}
+
+				if recv.Choices[0].Delta.ToolCalls != nil {
+					for _, toolCall := range recv.Choices[0].Delta.ToolCalls {
+						if toolCall.Function.Arguments != "" {
+							fmt.Print(toolCall.Function.Arguments)
+						}
+					}
+				}
+			} else if recv.Choices[0].Delta.Content != "" {
+				if isThinking {
+					log.Println("----- 模型最终回答 -----")
+					isThinking = false
+				}
+
+				fmt.Print(recv.Choices[0].Delta.Content)
+			}
 		}
 	}
 }
@@ -109,5 +138,5 @@ func main() {
 	config := LoadConfig()
 
 	test_standard_request(config)
-	test_stream_request(config)
+	// test_stream_request(config)
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,7 @@ module github.com/JackDrogon/aicodereader
 go 1.24.0
 
 require github.com/sashabaranov/go-openai v1.38.0
+
+// for deepseek reason, we need to use the following: https://github.com/goodenough227/go-openai/tree/master
+// require github.com/goodenough227/go-openai v0.0.0-20240328084325-098180978800
+replace github.com/sashabaranov/go-openai => github.com/goodenough227/go-openai v0.0.0-20250313060841-319a8ea883f9
